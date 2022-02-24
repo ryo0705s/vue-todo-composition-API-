b88adb4273a7b395db83e4ea9c0a471189d6f0bc
ryo0705s <inspire.sasaki15@gmail.com>
2022-02-24 15:18:19 +0900

    composition-apiを使ってhello worldを表示

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    {{ state.message }}
+  </div>
+</template>
+
+<script>
+import { reactive } from "vue";
+export default {
+  setup() {
+    const state = reactive({
+      message: "Hello Wold",
+    });
+    return {
+      state,
+    };
+  },
+};
+</script>
